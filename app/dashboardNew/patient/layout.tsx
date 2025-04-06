ac370e88356ac8985a87e42306b1d312865daddc
Kavindalgl <lakshithakavinda7@gmail.com>
2025-04-06 09:48:43 +0530

    Layout added

---
+++ app/dashboardNew/patient/layout.tsx
@@ -0,0 +1,21 @@
+import Sidebar from "@/app/dashboardNew/components/patient/patient-sidebar"
+import PageHeader from "@/app/dashboardNew/components/header"
+
+export default function PatientLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex min-h-screen bg-white">
+      {/* <Header /> */}
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <PageHeader />
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
