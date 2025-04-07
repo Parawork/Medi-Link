e2eb1dedc4f5ab72044e30bd992dc399a419e2cb
thivinu22 <thivinu.22@cse.mrt.ac.lk>
2025-04-07 21:26:28 +0530

    done

--- app/site/components/patient/signOutAction.ts
+++ app/site/components/patient/signOutAction.ts
@@ -1,9 +1,7 @@
 "use server";
 
 import { signOut } from "@/lib/auth";
-import { redirect } from "next/navigation";
 
 export async function handleSignOut() {
   await signOut();
-  redirect("/login");
-} 
+}
